304b552fa4280134a1d9f77d005f6ab5b841150f
Zeth Danielsson <zeda21@student.bth.se>
2023-05-16 10:03:38 +0200

    Renamed tests and added a cn.

--- test/test_minimal_paths.py
+++ test/test_minimal_paths.py
@@ -1,26 +1,76 @@
 import unittest
-
 from simulation.model import CommunicationNetwork
-from simulation.minimal_paths import single_source_dijkstra_vertices, single_source_dijkstra_hyperedges, DistanceType
+from simulation.minimal_paths import (
+    single_source_dijkstra_hyperedges,
+    single_source_dijkstra_vertices,
+    DistanceType,
+)
 
 
-class MinimalPath(unittest.TestCase):
-    cn = CommunicationNetwork({'h1': ['v1', 'v2'], 'h2': ['v2', 'v3'], 'h3': ['v3', 'v4']}, {'h1': 1, 'h2': 2, 'h3': 3})
+class self(unittest.TestCase):
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
+        self.cn = CommunicationNetwork(
+            {"h1": ["v1", "v2"], "h2": ["v2", "v3"], "h3": ["v3", "v4"]},
+            {"h1": 1, "h2": 2, "h3": 3},
+        )
+        self.cn2 = CommunicationNetwork({"h1": ["v1,v1"]}, {"h1": 0})
 
-    def test_1(self):
-        self.assertEqual(single_source_dijkstra_vertices(MinimalPath.cn, 'v1', DistanceType.SHORTEST, min_timing=0), {'v2': 1, 'v3': 2, 'v4': 3})
+    def test_short_correct(self):
+        self.assertEqual(
+            single_source_dijkstra_vertices(
+                self.cn, "v1", DistanceType.SHORTEST, min_timing=0
+            ),
+            {"v2": 1, "v3": 2, "v4": 3},
+        )
 
-    def test_2(self):
-        result_1 = single_source_dijkstra_vertices(MinimalPath.cn, 'v1', DistanceType.SHORTEST, min_timing=0)
-        result_2 = single_source_dijkstra_hyperedges(MinimalPath.cn, 'v1', DistanceType.SHORTEST, min_timing=0)
-        self.assertEqual(result_1, result_2, 'Single-source Dijkstra implementations are not equivalent')
+    def test_hyper_vert_shortest(self):
+        result_1 = single_source_dijkstra_vertices(
+            self.cn, "v1", DistanceType.SHORTEST, min_timing=0
+        )
+        result_2 = single_source_dijkstra_hyperedges(
+            self.cn, "v1", DistanceType.SHORTEST, min_timing=0
+        )
+        self.assertEqual(
+            result_1,
+            result_2,
+            "Single-source Dijkstra implementations are not equivalent",
+        )
 
-    def test_3(self):
-        result_1 = single_source_dijkstra_vertices(MinimalPath.cn, 'v1', DistanceType.FASTEST, min_timing=0)
-        result_2 = single_source_dijkstra_hyperedges(MinimalPath.cn, 'v1', DistanceType.FASTEST, min_timing=0)
-        self.assertEqual(result_1, result_2, 'Single-source Dijkstra implementations are not equivalent')
+    def test_hyper_vert_fastest(self):
+        result_1 = single_source_dijkstra_vertices(
+            self.cn, "v1", DistanceType.FASTEST, min_timing=0
+        )
+        result_2 = single_source_dijkstra_hyperedges(
+            self.cn, "v1", DistanceType.FASTEST, min_timing=0
+        )
+        self.assertEqual(
+            result_1,
+            result_2,
+            "Single-source Dijkstra implementations are not equivalent",
+        )
 
-    def test_4(self):
-        result_1 = single_source_dijkstra_vertices(MinimalPath.cn, 'v1', DistanceType.FOREMOST, min_timing=0)
-        result_2 = single_source_dijkstra_hyperedges(MinimalPath.cn, 'v1', DistanceType.FOREMOST, min_timing=0)
-        self.assertEqual(result_1, result_2, 'Single-source Dijkstra implementations are not equivalent')
+    def test_hyper_vert_foremost(self):
+        result_1 = single_source_dijkstra_vertices(
+            self.cn, "v1", DistanceType.FOREMOST, min_timing=0
+        )
+        result_2 = single_source_dijkstra_hyperedges(
+            self.cn, "v1", DistanceType.FOREMOST, min_timing=0
+        )
+        self.assertEqual(
+            result_1,
+            result_2,
+            "Single-source Dijkstra implementations are not equivalent",
+        )
+
+    def test_empy_network(self):
+        result1 = single_source_dijkstra_vertices(
+            self.cn, "v1", DistanceType.FASTEST, min_timing=0
+        )
+        result2 = single_source_dijkstra_vertices(
+            self.cn2, "v1", DistanceType.FASTEST, min_timing=0
+        )
+        self.assertNotEqual(result1, result2)
+
+
+unittest.main()
